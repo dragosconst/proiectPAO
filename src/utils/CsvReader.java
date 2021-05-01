cdb79823415fafbad84b30ade70d8522af547b03
dragosconst <38582034+dragosconst@users.noreply.github.com>
2021-05-01 15:05:39 +0300

    encapsulate magic constants in utils class

--- src/utils/CsvReader.java
+++ src/utils/CsvReader.java
@@ -19,26 +19,24 @@ import java.util.stream.Collectors;
 
 public class CsvReader {
     public static TreeSet<Carte> readCarti(Biblioteca biblioteca) {
-        // TODO: encapsulate the filepath in another util class or something
         String filePath = "C:\\Users\\Dragos\\IdeaProjects\\proiectPAO\\csv\\carti.csv";
         try(Scanner sc = new Scanner(new File(filePath))){
             TreeSet<Carte> carti = new TreeSet<>();
             while(sc.hasNextLine()) {
                 String line = sc.nextLine();
                 String[] dataLine = line.split(",");
-                // TODO: encapsulate this too
-                if(dataLine[0].equals("carteId")){
+                if(dataLine[CsvUtils.carteData.carteId].equals("carteId")){
                     continue;
                 }
                 else{
-                    Integer id = Integer.parseInt(dataLine[0]);
-                    Autor autor = biblioteca.getAutori().stream().filter(a -> a.getMembruId().toString().equals(dataLine[1])).findFirst().get();
-                    List<Sectiune> sectiuni = biblioteca.getSectiuni().stream().filter(s -> dataLine[2].contains(Integer.toString(s.getSectiuneId()))).collect(Collectors.toList());
-                    String denumire = dataLine[3];
-                    List<String> genuri = Arrays.stream(dataLine[4].split(";")).collect(Collectors.toList());
-                    Double pret = Double.parseDouble(dataLine[5]);
-                    Double discount = Double.parseDouble(dataLine[6]);
-                    int nrExemplare = Integer.parseInt(dataLine[7]);
+                    Integer id = Integer.parseInt(dataLine[CsvUtils.carteData.carteId]);
+                    Autor autor = biblioteca.getAutori().stream().filter(a -> a.getMembruId().toString().equals(dataLine[CsvUtils.carteData.autor])).findFirst().get();
+                    List<Sectiune> sectiuni = biblioteca.getSectiuni().stream().filter(s -> dataLine[CsvUtils.carteData.sectiuni].contains(Integer.toString(s.getSectiuneId()))).collect(Collectors.toList());
+                    String denumire = dataLine[CsvUtils.carteData.denumire];
+                    List<String> genuri = Arrays.stream(dataLine[CsvUtils.carteData.genuri].split(";")).collect(Collectors.toList());
+                    Double pret = Double.parseDouble(dataLine[CsvUtils.carteData.pret]);
+                    Double discount = Double.parseDouble(dataLine[CsvUtils.carteData.discount]);
+                    int nrExemplare = Integer.parseInt(dataLine[CsvUtils.carteData.nrExemplare]);
                     Carte carte = new Carte(id, autor, sectiuni, denumire, genuri, pret, discount, nrExemplare);
                     carti.add(carte);
                 }
@@ -51,26 +49,24 @@ public class CsvReader {
     }
 
     public static TreeSet<Autor> readAutori(Biblioteca biblioteca) {
-        // TODO: encapsulate the filepath in another util class or something
         String filePath = "C:\\Users\\Dragos\\IdeaProjects\\proiectPAO\\csv\\autori.csv";
         try(Scanner sc = new Scanner(new File(filePath))){
             TreeSet<Autor> autori = new TreeSet<>();
             while(sc.hasNextLine()) {
                 String line = sc.nextLine();
                 String[] dataLine = line.split(",");
-                // TODO: encapsulate this too
-                if(dataLine[0].equals("autorId")){
+                if(dataLine[CsvUtils.autorData.autorId].equals("autorId")){
                     continue;
                 }
                 else{
-                    Integer id = Integer.parseInt(dataLine[0]);
-                    String nume = dataLine[1];
-                    String prenume = dataLine[2];
-                    String statut = dataLine[3];
-                    Date dataInscriere = new SimpleDateFormat("dd/MM/yyyy").parse(dataLine[4]);
-                    Double popularitate = Double.parseDouble(dataLine[5]);
-                    Double discount = Double.parseDouble(dataLine[6]);
-                    String descriere = dataLine[7];
+                    Integer id = Integer.parseInt(dataLine[CsvUtils.autorData.autorId]);
+                    String nume = dataLine[CsvUtils.autorData.nume];
+                    String prenume = dataLine[CsvUtils.autorData.prenume];
+                    String statut = dataLine[CsvUtils.autorData.statut];
+                    Date dataInscriere = new SimpleDateFormat("dd/MM/yyyy").parse(dataLine[CsvUtils.autorData.dataInscriere]);
+                    Double popularitate = Double.parseDouble(dataLine[CsvUtils.autorData.popularitate]);
+                    Double discount = Double.parseDouble(dataLine[CsvUtils.autorData.discount]);
+                    String descriere = dataLine[CsvUtils.autorData.descriere];
                     Autor autor = new Autor(id, nume, prenume, statut, dataInscriere, popularitate, discount, descriere);
                     autori.add(autor);
                 }
@@ -83,34 +79,32 @@ public class CsvReader {
     }
 
     public static HashSet<Angajat> readAngajati(Biblioteca biblioteca) {
-        // TODO: encapsulate the filepath in another util class or something
         String filePath = "C:\\Users\\Dragos\\IdeaProjects\\proiectPAO\\csv\\angajati.csv";
         try(Scanner sc = new Scanner(new File(filePath))){
             HashSet<Angajat> angajati = new HashSet<>();
             while(sc.hasNextLine()) {
                 String line = sc.nextLine();
                 String[] dataLine = line.split(",");
-                // TODO: encapsulate this too
-                if(dataLine[0].equals("angajatId")){
+                if(dataLine[CsvUtils.angajatData.angajatId].equals("angajatId")){
                     continue;
                 }
                 else{
-                    Integer id = Integer.parseInt(dataLine[0]);
-                    String nume = dataLine[1];
-                    String prenume = dataLine[2];
-                    String statut = dataLine[3];
-                    Date dataInscriere = new SimpleDateFormat("dd/MM/yyyy").parse(dataLine[4]);
-                    Double salariu = Double.parseDouble(dataLine[5]);
-                    Double commission = Double.parseDouble(dataLine[6]);
-                    Integer type = Integer.parseInt(dataLine[7]);
+                    Integer id = Integer.parseInt(dataLine[CsvUtils.angajatData.angajatId]);
+                    String nume = dataLine[CsvUtils.angajatData.nume];
+                    String prenume = dataLine[CsvUtils.angajatData.prenume];
+                    String statut = dataLine[CsvUtils.angajatData.statut];
+                    Date dataInscriere = new SimpleDateFormat("dd/MM/yyyy").parse(dataLine[CsvUtils.angajatData.dataInscriere]);
+                    Double salariu = Double.parseDouble(dataLine[CsvUtils.angajatData.salariu]);
+                    Double commission = Double.parseDouble(dataLine[CsvUtils.angajatData.commission]);
+                    Integer type = Integer.parseInt(dataLine[CsvUtils.angajatData.type]);
                     if (type == 2) {
-                        List<String> servere = Arrays.stream(dataLine[8].split(";")).collect(Collectors.toList());
+                        List<String> servere = Arrays.stream(dataLine[CsvUtils.angajatData.servere].split(";")).collect(Collectors.toList());
                         ITist iTist = new ITist(nume, prenume, statut, dataInscriere, salariu, commission, servere);
                         angajati.add(iTist);
                     }
                     else{
-                        List<Sectiune> sectiuni = biblioteca.getSectiuni().stream().filter(s -> dataLine[9].contains(Integer.toString(s.getSectiuneId()))).collect(Collectors.toList());
-                        Date dataNasterii = new SimpleDateFormat("dd/MM/yyyy").parse(dataLine[10]);
+                        List<Sectiune> sectiuni = biblioteca.getSectiuni().stream().filter(s -> dataLine[CsvUtils.angajatData.sectiuni].contains(Integer.toString(s.getSectiuneId()))).collect(Collectors.toList());
+                        Date dataNasterii = new SimpleDateFormat("dd/MM/yyyy").parse(dataLine[CsvUtils.angajatData.dataNasterii]);
                         Bibliotecar bibliotecar = new Bibliotecar(nume, prenume, statut, dataInscriere, salariu, commission, sectiuni,dataNasterii);
                         angajati.add(bibliotecar);
                     }
@@ -125,21 +119,19 @@ public class CsvReader {
     }
 
     public static HashSet<Aripa> readAripi(Biblioteca biblioteca) {
-        // TODO: encapsulate the filepath in another util class or something
         String filePath = "C:\\Users\\Dragos\\IdeaProjects\\proiectPAO\\csv\\aripi.csv";
         try(Scanner sc = new Scanner(new File(filePath))){
             HashSet<Aripa> aripi = new HashSet<>();
             while(sc.hasNextLine()) {
                 String line = sc.nextLine();
                 String[] dataLine = line.split(",");
-                // TODO: encapsulate this too
-                if(dataLine[0].equals("aripaid")){
+                if(dataLine[CsvUtils.aripaData.aripaId].equals("aripaid")){
                     continue;
                 }
                 else{
-                    Integer id = Integer.parseInt(dataLine[0]);
-                    String denumire = dataLine[1];
-                    Double discount = Double.parseDouble(dataLine[2]);
+                    Integer id = Integer.parseInt(dataLine[CsvUtils.aripaData.aripaId]);
+                    String denumire = dataLine[CsvUtils.aripaData.denumire];
+                    Double discount = Double.parseDouble(dataLine[CsvUtils.aripaData.discount]);
                     Aripa aripa = new Aripa(id, denumire, discount);
                     aripi.add(aripa);
                 }
@@ -153,24 +145,22 @@ public class CsvReader {
     }
 
     public static HashSet<Sectiune> readSectiuni(Biblioteca biblioteca) {
-        // TODO: encapsulate the filepath in another util class or something
         String filePath = "C:\\Users\\Dragos\\IdeaProjects\\proiectPAO\\csv\\sectiuni.csv";
         try(Scanner sc = new Scanner(new File(filePath))){
             HashSet<Sectiune> sectiuni = new HashSet<>();
             while(sc.hasNextLine()) {
                 String line = sc.nextLine();
                 String[] dataLine = line.split(",");
-                // TODO: encapsulate this too
-                if(dataLine[0].equals("sectiuneid")){
+                if(dataLine[CsvUtils.sectiuneData.sectiuneId].equals("sectiuneid")){
                     continue;
                 }
                 else{
-                    Integer id = Integer.parseInt(dataLine[0]);
-                    Integer aripaId = Integer.parseInt(dataLine[1]);
+                    Integer id = Integer.parseInt(dataLine[CsvUtils.sectiuneData.sectiuneId]);
+                    Integer aripaId = Integer.parseInt(dataLine[CsvUtils.sectiuneData.aripaId]);
                     Aripa aripa = biblioteca.getAripi().stream().filter(a -> a.getAripaId() == aripaId).findFirst().get();
-                    String denumire = dataLine[2];
-                    List<String> genuri = Arrays.stream(dataLine[3].split(";")).collect(Collectors.toList());
-                    Double discount = Double.parseDouble(dataLine[4]);
+                    String denumire = dataLine[CsvUtils.sectiuneData.denumire];
+                    List<String> genuri = Arrays.stream(dataLine[CsvUtils.sectiuneData.genuri].split(";")).collect(Collectors.toList());
+                    Double discount = Double.parseDouble(dataLine[CsvUtils.sectiuneData.discount]);
                     Sectiune sectiune = new Sectiune(id, aripa, denumire, genuri, discount);
                     sectiuni.add(sectiune);
                 }
--- src/utils/CsvUtils.java
+++ src/utils/CsvUtils.java
@@ -8,4 +8,54 @@ class CsvUtils {
     public static final String angajati = "angajati";
     public static final String aripi = "aripi";
     public static final String sectiuni = "sectiuni";
+
+    public static class carteData {
+        public static final Integer carteId = 0;
+        public static final Integer autor = 1;
+        public static final Integer sectiuni = 2;
+        public static final Integer denumire = 3;
+        public static final Integer genuri = 4;
+        public static final Integer pret = 5;
+        public static final Integer discount = 6;
+        public static final Integer nrExemplare = 7;
+    }
+
+    public static class autorData {
+        public static final Integer autorId = 0;
+        public static final Integer nume = 1;
+        public static final Integer prenume = 2;
+        public static final Integer statut = 3;
+        public static final Integer dataInscriere = 4;
+        public static final Integer popularitate = 5;
+        public static final Integer discount = 6;
+        public static final Integer descriere = 7;
+    }
+
+    public static class angajatData {
+        public static final Integer angajatId = 0;
+        public static final Integer nume = 1;
+        public static final Integer prenume = 2;
+        public static final Integer statut = 3;
+        public static final Integer dataInscriere = 4;
+        public static final Integer salariu = 5;
+        public static final Integer commission = 6;
+        public static final Integer type = 7;
+        public static final Integer servere = 8;
+        public static final Integer sectiuni = 9;
+        public static final Integer dataNasterii = 10;
+    }
+
+    public static class aripaData {
+        public static final Integer aripaId = 0;
+        public static final Integer denumire = 1;
+        public static final Integer discount = 2;
+    }
+
+    public static class sectiuneData {
+        public static final Integer sectiuneId = 0;
+        public static final Integer aripaId = 1;
+        public static final Integer denumire = 2;
+        public static final Integer genuri = 3;
+        public static final Integer discount = 4;
+    }
 }
